06c987bb50044259f4dcd18c21f6f522639e0aea
Ritika Shakya <ritikashakya31@gmail.com>
2025-01-11 01:10:01 +0530

    category detials page add

---
+++ category-details.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Festival App</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="sty.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        
+        <!-- Header -->
+        <!-- Calendar Filter Section -->
+        <div class="mb-4 mt-3 flex items-center justify-between">
+            <span style="font-weight: 500;font-size: 21px;">Accounting</span>
+            <button class="p-2 border rounded" style="background: red;color: white; border-radius: 21px; padding: 8px 17px;">
+                <i class="fa fa-edit"></i>Change Category
+            </button>
+        </div>
+        <div id="section123-search-box" class="mb-4 flex items-center">
+            <input type="text" placeholder="Select Category and Images.." class="p-2 border rounded flex-grow">
+        </div>
+        <div id="section123-filter-dropdown">
+            <select>
+                <option>Select Sub Category</option>
+                <option>Sub Category1</option>
+                <option>Sub Category2</option>
+                <option>Sub Category3 </option>
+                <option>Sub Category4</option>
+            </select>
+        </div>
+        <!-- Festival Images Sections -->
+        <div id="section123-vasant-panchami">
+            <div class="header flex items-center justify-between mb-6">
+                <h2 class="text-2xl font-bold other-h2">Auditor</h2>
+                <!-- <div class="flex-grow border-t border-gray-300 mx-4"></div> -->
+                <a href="" class="text-black-500 hover:underline">See all <i class="fa fa-chevron-right"
+                        aria-hidden="true"></i></a>
+            </div>
+            <div class="card-container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/1.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" style="width:90px;" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/2.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/3.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" style="width:90px;" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/4.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/1.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" style="width:90px;" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/2.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/3.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+            </div>
+
+        </div>
+
+        <div id="section123-vasant-panchami">
+            <div class="header flex items-center justify-between mb-6">
+                <h2 class="text-2xl font-bold other-h2">Accounting Firm</h2>
+                <div class="flex-grow border-t border-gray-300 mx-4"></div>
+                <a href="" class="text-black-500 hover:underline">See all <i class="fa fa-chevron-right"
+                        aria-hidden="true"></i></a>
+            </div>
+            <div
+                class="card-container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 p-4">
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/republic/1.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/republic/2.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/republic/3.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/republic/4.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/republic/1.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/republic/2.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+            </div>
+
+        </div>
+        <div id="section123-vasant-panchami">
+            <div class="header flex items-center justify-between mb-6">
+                <h2 class="text-2xl font-bold other-h2">Chartant Accountant</h2>
+                <div class="flex-grow border-t border-gray-300 mx-4"></div>
+                <a href="" class="text-black-500 hover:underline">See all <i class="fa fa-chevron-right"
+                        aria-hidden="true"></i></a>
+            </div>
+            <div class="card-container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/1.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" style="width:90px;" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/2.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/3.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" style="width:90px;" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/4.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/1.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" style="width:90px;" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/2.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+                <a href="" class="card bg-white rounded-lg shadow hover:shadow-lg overflow-hidden">
+                    <img src="assets/img/makar sankrantri/3.png" alt="Vasant Panchami Image" class="w-full h-auto">
+                </a>
+            </div>
+
+        </div>
+        <?php
+        include 'nav.php';
+        ?>
+
+      
+</body>
+
+</html>
